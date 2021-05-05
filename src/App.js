acf78fbe72fa1edc03629ad35cadd7a649d1d02d
Ismail Dinar <sm41l.4796@gmail.com>
2021-05-05 21:25:27 +0100

    Style the heading and add new task button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Header from './components/header/Header';
 import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-    </div>
+    <Container maxWidth='lg'>
+      <Box display='flex' flexDirection='row' justifyContent='center'>
+        <Header />
+      </Box>
+    </Container>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,26 @@
+import Button from '@material-ui/core/Button';
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import Box from '@material-ui/core/Box';
+
 const Header = () => (
-  <header>
-    <h1>Task Tracker</h1>
+  <header className='w-50'>
+    <Box
+      display='flex'
+      flexDirection='row'
+      justifyContent='space-between'
+      alignItems='center'
+    >
+      <h1>Task Tracker</h1>
+      <Button
+        variant='contained'
+        color='primary'
+        size='small'
+        startIcon={<LibraryAddIcon />}
+        disableElevation
+      >
+        New Task
+      </Button>
+    </Box>
   </header>
 );
 
