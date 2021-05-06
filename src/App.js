62ce1f96d5ea6a323bbf4f0b4f33d85fadee005f
Ismail Dinar <sm41l.4796@gmail.com>
2021-05-06 20:58:08 +0100

    Render some dummy tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import {useState} from 'react';
+import Tasks from './components/tasks/Tasks';
 import Header from './components/header/Header';
+
 import './App.scss';
 
-function App() {
+const App = () => {
+  const [tasks] = useState([
+    {
+      id: 1,
+      text: 'Task 1',
+      createdAt: new Date(),
+      dueDate: new Date(),
+    },
+    {
+      id: 2,
+      text: 'Task 2',
+      createdAt: new Date(),
+      dueDate: new Date(),
+    },
+    {
+      id: 3,
+      text: 'Task 3',
+      createdAt: new Date(),
+      dueDate: new Date(),
+    },
+    {
+      id: 4,
+      text: 'Task 4',
+      createdAt: new Date(),
+      dueDate: new Date(),
+    }
+  ]);
+
   return (
     <Container maxWidth='lg'>
       <Box display='flex' flexDirection='row' justifyContent='center'>
         <Header />
       </Box>
+      <Tasks className='w-50' tasks={tasks} />
     </Container>
   );
-}
+};
 
 export default App;
